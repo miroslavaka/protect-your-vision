cf7bf432c30e3faafb99023b39829a31cb803cc7
Miroslava <kacmarova.miroslava@gmail.com>
2022-01-16 18:08:36 +0100

    add countdown counter

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ let btnThree = document.querySelector('.button-three');
 let myAudio = document.querySelector('.my-audio');
 let isPlaying = false;
 
+function changeBackgroundImage(bgImage) {
+    document.body.style.backgroundImage = 'url(' + bgImage + ')';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = 'cover';
+}
+
+//DOMContentLoaded event is triggered when your whole HTML page has been loaded
+//document.addEventListener('DOMContentLoaded', changeBackgroundImage());
+
+//if you want to wait until page is fully loaded, you can listen for load
+document.addEventListener('DOMContentLoaded', changeBackgroundImage());
+
 myAudio.onplaying = function() {
     isPlaying = true;
 };
@@ -50,4 +62,32 @@ function playMusicBtnThree() {
 
 function togglePlay() {
     isPlaying === true ? myAudio.pause() : playMusicBtnThree();
+}
+
+//countdown
+const startingMinutes = 40;
+let time = startingMinutes * 60;
+const countdownEl = document.querySelector('.countdown');
+
+let runTheClock = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    /*  if (countdownEl.innerHTML == '0:00') {
+                countdownEl.innerHTML === '0:00';
+                myAudio.play();
+            } */
+    if (--time < 0) {
+        clearInterval(runTheClock);
+        myAudio.play();
+    }
+
+    /* if (minutes === 0) {
+                  myAudio.play();
+              } */
 }
